27945bf2c21bd73846cfa6afd023346ead7bea55
Pedro Leite <pedrocaleite@gmail.com>
2020-09-04 20:45:57 -0300

    Starting GUI

--- Scabble/GUI.py
+++ Scabble/GUI.py
@@ -1,7 +1,18 @@
 from tkinter import *
+from Scrabble_v2 import *
 
 root = Tk()
 
+e = Entry(root, width=50)
+e.pack()
+
+def myClick():
+    myLabel2 = Label(root, text=e.get())
+    myLabel2.pack()
+
+
+myButton = Button(root, text="Enter Your Name", command=myClick, bg="black", fg="white")
+myButton.pack()
 # Creating a label widget
 myLabel = Label(root, text="Hello World!")
 #shoving it onto the screen
